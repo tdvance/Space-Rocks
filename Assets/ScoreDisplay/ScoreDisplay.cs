5cb5d56854712ca470bf1be6f72117b29208efb1
tdvance <tdvance@gmail.com>
2016-11-15 21:48:02 -0500

    Wire up buttons with Buttons.cs script Tweak rocks Add score displays to menu page prefab everything

---
+++ Assets/ScoreDisplay/ScoreDisplay.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public string prefixText = "Score: ";
+    public int score = 0;
+    public Color highlightColor = Color.red;
+    public bool highlight;
+    public float highlightTime = 1.5f;
+
+
+    private Text scoreText;
+    private Color textColor = Color.green;
+    private float highlightAmount = 1f;
+
+
+    // Use this for initialization
+    void Start() {
+        scoreText = GetComponentInChildren<Text>();
+        scoreText.text = prefixText + score.ToString();
+        textColor = scoreText.color;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        scoreText.text = prefixText + score.ToString();
+        if (highlight) {
+            if (highlightAmount > 0) {
+                scoreText.color = textColor*(1f-highlightAmount) + highlightColor*highlightAmount;
+                highlightAmount -= Time.deltaTime / highlightTime;
+            }else {
+                highlight = false;
+                highlightAmount = 1f;
+                scoreText.color = textColor;
+            }
+        }
+    }
+
+    public void Highlight() {
+        highlight = true;
+        highlightAmount = 1f;
+    }
+
+    public void Advance(int amount) {
+        score += amount;
+    }
+
+}
---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+    public void SubmitStart() {
+        FlexibleMusicManager.instance.Pause();
+        LevelManager.instance.ChangeState(LevelManager.GameState.GAME, 0.5f);
+    }
+
+    public void SubmitQuit() {
+        Application.Quit();
+    }
+
+    public void SubmitOptions() {
+        LevelManager.instance.ChangeState(LevelManager.GameState.OPTIONS, 0.5f);
+    }
+
+    public void SubmitResumeMenu() {
+        LevelManager.instance.ChangeState(LevelManager.GameState.MENU, 0.5f);
+    }
+
+    public void SubmitExitGame() {
+        //TODO call end of game method of game class
+        FlexibleMusicManager.instance.Pause();
+        LevelManager.instance.ChangeState(LevelManager.GameState.MENU, 0.5f);
+    }
+
+    public void SubmitOptionsFromGame() {
+        //TODO pause game, then load options menu from game additively
+    }
+
+    public void SubmitResumeGame() {
+        //TODO destroy options menu from game and resume game from pause
+    }
+
+}
