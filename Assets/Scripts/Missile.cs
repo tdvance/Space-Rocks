e4a9f863fee490602baecbd404fbf9b1a31d3065
tdvance <tdvance@gmail.com>
2016-11-16 12:23:43 -0500

    Created Game Scene

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+    public float speed = 50;
+    public float timeToLive = 1.5f;
+
+
+    private Rigidbody2D rb;
+    private float startTime;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(Time.time-startTime > timeToLive) {
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Ship : MonoBehaviour {
+    public float accelerationFactor = 1f;
+    public float turnSpeed = 1f;
+
+    public float frontDistance = 0.456f;
+    public GameObject missilePrefab;
+
+
+    private float accelerate;
+    private float turn;
+    private Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        turn = CrossPlatformInputManager.GetAxis("Horizontal");
+        accelerate = CrossPlatformInputManager.GetAxis("Vertical");
+        if (CrossPlatformInputManager.GetButtonDown("Fire1")) {
+            Fire();
+        }
+    }
+
+    void FixedUpdate() {
+        rb.angularVelocity = -turn * turnSpeed;
+        rb.AddForce(accelerate * accelerationFactor*transform.up);
+    }
+
+    void Fire() {
+        Instantiate(missilePrefab, transform.position + transform.up * frontDistance, transform.localRotation);
+    }
+}
